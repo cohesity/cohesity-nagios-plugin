e86d5ba8d75837d2d4096719bf5d66860ecf6d0f
Christina Mudarth <51001476+christina1999@users.noreply.github.com>
2019-06-27 16:47:01 -0400

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Copyright 2019 Cohesity Inc.
+# Author : Christina Mudarth <christina.mudarth@cohesity.com>
+# Usage :
+# write credentials as a string
+
+ip = 'IP'
+username = 'USERNAME'
+password = 'PASSWORD'
+domain = 'DOMAIN'
